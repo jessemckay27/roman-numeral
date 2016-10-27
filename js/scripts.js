28750ad9fd7db6c7a7f81a11060bcc19c20b92f2
Jesse McKay and Levi Bibo <levi@>
2016-10-27 11:24:55 -0700

    refactor to reduce conversion functions to one function; old functions left commented out.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
+var symbolArray = [["I", "V", "X"], ["X", "L", "C"], ["C", "D", "M"], ["M"]];
+
 function numeralControl(originalNumber) {
   var alteredNumber = originalNumber;
   var numeralArray = [];
   if (alteredNumber % 10 != 0) {
     var onesPlace = alteredNumber % 10;
-    numeralArray.push(onesDigitConverter(onesPlace));
+    numeralArray.push(digitConverter(onesPlace, symbolArray[0]));
     alteredNumber -= onesPlace;
   };
   if (alteredNumber % 100 != 0) {
     var tensPlace = alteredNumber % 100;
-    numeralArray.push(tensDigitConverter(tensPlace));
+    numeralArray.push(digitConverter(tensPlace / 10, symbolArray[1]));
     alteredNumber -= tensPlace;
   };
   if (alteredNumber % 1000 != 0) {
     var hundredsPlace = alteredNumber % 1000;
-    numeralArray.push(hundredsDigitConverter(hundredsPlace));
+    numeralArray.push(digitConverter(hundredsPlace / 100, symbolArray[2]));
     alteredNumber -= hundredsPlace;
   };
   if (alteredNumber % 10000 != 0 && alteredNumber < 4000) {
     var thousandsPlace = alteredNumber % 10000;
-    numeralArray.push(thousandsDigitConverter(thousandsPlace));
+    numeralArray.push(digitConverter(thousandsPlace / 1000, symbolArray[3]));
   } else if (alteredNumber >= 4000) {
     numeralArray = ["Please enter a number smaller than 4000"];
   }
@@ -27,83 +29,106 @@ function numeralControl(originalNumber) {
   return numeralArray.join("");
 }
 
-function onesDigitConverter(onesPlace) {
-  var onesDigits = [];
-  if (onesPlace < 4) {
-    for (i = 0; i < onesPlace; i++) {
-      onesDigits.push("I");
+function digitConverter(number, symbols) {
+  var digits = [];
+  if (number < 4) {
+    for (i = 0; i < number; i++) {
+      digits.push(symbols[0]);
     };
 
-  } else if (onesPlace === 4) {
-    onesDigits.push("IV");
+  } else if (number === 4) {
+    digits.push(symbols[0] + symbols[1]);
 
-  } else if (5 <= onesPlace && onesPlace < 9) {
-    onesDigits.push("V");
-    for (i = 5; i < onesPlace; i++) {
-      onesDigits.push("I");
+  } else if (5 <= number && number < 9) {
+    digits.push(symbols[1]);
+    for (i = 5; i < number; i++) {
+      digits.push(symbols[0]);
     };
 
-  } else if (onesPlace === 9) {
-    onesDigits.push("IX");
-  };
+  } else if (number === 9) {
+    digits.push(symbols[0] + symbols[2]);
+  }
 
-  return onesDigits.join("");
+  return digits.join("");
 }
 
-function tensDigitConverter(tensPlace) {
-  var tensDigits = [];
-  if (tensPlace < 40) {
-    for (i = 0; i < tensPlace; i += 10) {
-      tensDigits.push("X");
-    };
-
-  } else if (tensPlace === 40) {
-    tensDigits.push("XL");
-
-  } else if (50 <= tensPlace && tensPlace < 90) {
-    tensDigits.push("L");
-    for (i = 50; i < tensPlace; i += 10) {
-      tensDigits.push("X");
-    };
-
-  } else if (tensPlace === 90) {
-    tensDigits.push("XC");
-  };
-
-  return tensDigits.join("");
-}
-
-function hundredsDigitConverter(hundredsPlace) {
-  var hundredsDigits = [];
-  if (hundredsPlace < 400) {
-    for (i = 0; i < hundredsPlace; i += 100) {
-      hundredsDigits.push("C");
-    };
-
-  } else if (hundredsPlace === 400) {
-    hundredsDigits.push("CD");
-
-  } else if (500 <= hundredsPlace && hundredsPlace < 900) {
-    hundredsDigits.push("D");
-    for (i = 500; i < hundredsPlace; i += 100) {
-      hundredsDigits.push("C");
-    };
-
-  } else if (hundredsPlace === 900) {
-    hundredsDigits.push("CM");
-  };
-
-  return hundredsDigits.join("");
-}
-
-function thousandsDigitConverter(thousandsPlace) {
-  var thousandsDigits = [];
-  for (i = 0; i < thousandsPlace; i += 1000) {
-    thousandsDigits.push("M");
-  };
-
-  return thousandsDigits.join("");
-}
+// function onesDigitConverter(onesPlace) {
+//   var onesDigits = [];
+//   if (onesPlace < 4) {
+//     for (i = 0; i < onesPlace; i++) {
+//       onesDigits.push("I");
+//     };
+//
+//   } else if (onesPlace === 4) {
+//     onesDigits.push("IV");
+//
+//   } else if (5 <= onesPlace && onesPlace < 9) {
+//     onesDigits.push("V");
+//     for (i = 5; i < onesPlace; i++) {
+//       onesDigits.push("I");
+//     };
+//
+//   } else if (onesPlace === 9) {
+//     onesDigits.push("IX");
+//   };
+//
+//   return onesDigits.join("");
+// }
+//
+// function tensDigitConverter(tensPlace) {
+//   var tensDigits = [];
+//   if (tensPlace < 40) {
+//     for (i = 0; i < tensPlace; i += 10) {
+//       tensDigits.push("X");
+//     };
+//
+//   } else if (tensPlace === 40) {
+//     tensDigits.push("XL");
+//
+//   } else if (50 <= tensPlace && tensPlace < 90) {
+//     tensDigits.push("L");
+//     for (i = 50; i < tensPlace; i += 10) {
+//       tensDigits.push("X");
+//     };
+//
+//   } else if (tensPlace === 90) {
+//     tensDigits.push("XC");
+//   };
+//
+//   return tensDigits.join("");
+// }
+//
+// function hundredsDigitConverter(hundredsPlace) {
+//   var hundredsDigits = [];
+//   if (hundredsPlace < 400) {
+//     for (i = 0; i < hundredsPlace; i += 100) {
+//       hundredsDigits.push("C");
+//     };
+//
+//   } else if (hundredsPlace === 400) {
+//     hundredsDigits.push("CD");
+//
+//   } else if (500 <= hundredsPlace && hundredsPlace < 900) {
+//     hundredsDigits.push("D");
+//     for (i = 500; i < hundredsPlace; i += 100) {
+//       hundredsDigits.push("C");
+//     };
+//
+//   } else if (hundredsPlace === 900) {
+//     hundredsDigits.push("CM");
+//   };
+//
+//   return hundredsDigits.join("");
+// }
+//
+// function thousandsDigitConverter(thousandsPlace) {
+//   var thousandsDigits = [];
+//   for (i = 0; i < thousandsPlace; i += 1000) {
+//     thousandsDigits.push("M");
+//   };
+//
+//   return thousandsDigits.join("");
+// }
 
 
 $(document).ready(function(){

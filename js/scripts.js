6c11956b80a31dd5cf92003e9cbe890bc001daff
Jesse McKay and Levi Bibo <levi@>
2016-10-27 11:27:45 -0700

    removed commented functions, keeping refactored function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//
 var symbolArray = [["I", "V", "X"], ["X", "L", "C"], ["C", "D", "M"], ["M"]];
 
 function numeralControl(originalNumber) {
@@ -52,85 +53,8 @@ function digitConverter(number, symbols) {
   return digits.join("");
 }
 
-// function onesDigitConverter(onesPlace) {
-//   var onesDigits = [];
-//   if (onesPlace < 4) {
-//     for (i = 0; i < onesPlace; i++) {
-//       onesDigits.push("I");
-//     };
-//
-//   } else if (onesPlace === 4) {
-//     onesDigits.push("IV");
-//
-//   } else if (5 <= onesPlace && onesPlace < 9) {
-//     onesDigits.push("V");
-//     for (i = 5; i < onesPlace; i++) {
-//       onesDigits.push("I");
-//     };
-//
-//   } else if (onesPlace === 9) {
-//     onesDigits.push("IX");
-//   };
-//
-//   return onesDigits.join("");
-// }
-//
-// function tensDigitConverter(tensPlace) {
-//   var tensDigits = [];
-//   if (tensPlace < 40) {
-//     for (i = 0; i < tensPlace; i += 10) {
-//       tensDigits.push("X");
-//     };
-//
-//   } else if (tensPlace === 40) {
-//     tensDigits.push("XL");
-//
-//   } else if (50 <= tensPlace && tensPlace < 90) {
-//     tensDigits.push("L");
-//     for (i = 50; i < tensPlace; i += 10) {
-//       tensDigits.push("X");
-//     };
-//
-//   } else if (tensPlace === 90) {
-//     tensDigits.push("XC");
-//   };
-//
-//   return tensDigits.join("");
-// }
-//
-// function hundredsDigitConverter(hundredsPlace) {
-//   var hundredsDigits = [];
-//   if (hundredsPlace < 400) {
-//     for (i = 0; i < hundredsPlace; i += 100) {
-//       hundredsDigits.push("C");
-//     };
-//
-//   } else if (hundredsPlace === 400) {
-//     hundredsDigits.push("CD");
-//
-//   } else if (500 <= hundredsPlace && hundredsPlace < 900) {
-//     hundredsDigits.push("D");
-//     for (i = 500; i < hundredsPlace; i += 100) {
-//       hundredsDigits.push("C");
-//     };
-//
-//   } else if (hundredsPlace === 900) {
-//     hundredsDigits.push("CM");
-//   };
-//
-//   return hundredsDigits.join("");
-// }
-//
-// function thousandsDigitConverter(thousandsPlace) {
-//   var thousandsDigits = [];
-//   for (i = 0; i < thousandsPlace; i += 1000) {
-//     thousandsDigits.push("M");
-//   };
-//
-//   return thousandsDigits.join("");
-// }
 
-
+//
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
